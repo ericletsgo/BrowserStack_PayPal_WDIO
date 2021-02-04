96487e1783120bd6e54b4b7c76314524c33f5e71
Eric Wang <matchstik@MacBook-Pro.local>
2021-02-03 17:00:12 -0800

    added gitignore

--- node_modules/@wdio/local-runner/build/run.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="node" />
-interface RunnerInterface extends NodeJS.EventEmitter {
-    sigintWasCalled: boolean;
-    [key: string]: any;
-}
-export declare const runner: RunnerInterface;
-export declare const exitHookFn: (callback: () => void) => void;
-export {};
-//# sourceMappingURL=run.d.ts.map
