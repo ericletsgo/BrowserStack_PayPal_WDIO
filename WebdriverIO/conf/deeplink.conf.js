8307322ffd05073b40a345a903484be8c5e275af
Charlie Lee <clcblee@gmail.com>
2021-02-16 19:31:58 -0800

    Placeholder deeplink script w/ Wiki app

---
+++ WebdriverIO/conf/deeplink.conf.js
@@ -0,0 +1,39 @@
+exports.config = {
+  user: process.env.BROWSERSTACK_USERNAME,
+  key: process.env.BROWSERSTACK_ACCESS_KEY,
+
+  updateJob: false,
+  specs: [
+    './test/specs/deeplink.js'
+  ],
+  exclude: [],
+
+  capabilities: [{
+    project: "PayPal SDK",
+    build: 'PayPal Deep Linking',
+    device: 'Google Pixel 3',
+    os_version: "9.0",
+    app: process.env.BROWSERSTACK_APP_ID
+  }],
+
+  coloredLogs: true,
+  screenshotPath: './errorShots/',
+  baseUrl: '',
+  waitforTimeout: 60*1000,
+  connectionRetryTimeout: 90*1000,
+  connectionRetryCount: 3,
+
+  logLevels: {
+    '@wdio/browserstack-service': 'info'
+  },
+
+  services: ['browserstack'],
+
+  framework: 'mocha',
+
+  mochaOpts: {
+    ui: 'bdd',
+    timeout: 60*1000
+  },
+
+};
---
+++ WebdriverIO/test/specs/deeplink.js
@@ -0,0 +1,39 @@
+const expectChai = require('chai').expect;
+
+describe('Deep Link', () => {
+    it('can switch between app and browser', () => {
+
+        var chromePackage = "com.android.chrome";
+        var chromeActivity = "com.google.android.apps.chrome.Main";
+
+        var appPackage = driver.getCurrentPackage();
+        var appActivity = driver.getCurrentActivity();
+
+        driver.startActivity(chromePackage, chromeActivity);
+
+        driver.waitUntil(
+            () => driver.getContexts().length > 1, {
+                timeout: 5000,
+                timeoutMsg: 'expected WEBVIEW_chrome to become available'
+            }
+        )
+        driver.switchContext('WEBVIEW_chrome');
+
+        driver.url('https://paypal.github.io/react-paypal-js/iframe.html?id=example-venmobutton--horizontal&viewMode=story');
+
+        const frame = $('/html/body/div[3]/div/div/iframe[1]');
+        frame.waitForExist({timeout: 5000, timeoutMsg: 'expected iframe to appear'});
+        browser.switchToFrame(frame);
+
+        const payPalButton = $('/html/body/div[1]/div/div[1]/div');
+        payPalButton.click();
+
+        driver.pause(3000);
+
+        driver.startActivity(appPackage, appActivity);
+
+        var currentActivity = driver.getCurrentActivity();
+        expectChai(currentActivity).to.equal(appActivity); 
+        
+    });
+});
